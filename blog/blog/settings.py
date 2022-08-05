2104a81e92e5ccbcf0edaabc00deeae5a3be407c
liangjiaxing996 <2479140464@qq.com>
2022-08-05 12:51:09 +0800

    '静态资源配置'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -116,6 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#设置静态资源的路径
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
+
 
 #redis的配置
 CACHES = {
